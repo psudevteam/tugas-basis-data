eb8658ced85f0c125fe78bec62a05e4de7559b98
Maulana Sodiqin <sodiqincahayana1@gmail.com>
2021-11-21 23:02:38 +0700

    Maulana Sodiqin | Add VueJs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,212 +1,281 @@
-function getId(id, isi) {
-  return (document.getElementById(id).innerHTML = isi);
-}
+const app = Vue.createApp({
+  data() {
+    return {
+      dataMahasiswa: [],
+      dataDosen: [],
+      dataMatkul: [],
+      dataFakultas: [],
+      isBannerShow: true,
+      isTableShow: false,
+      isTableDosenShow: false,
+      isTableMahasiswaShow: false,
+      isTableMatakuliahShow: false,
+      isTableFakultasShow: false,
+      isEditDosenShow: false,
+      isEditMahasiswaShow: false,
+      isEditMatakuliahShow: false,
+      isEditFakultasShow: false,
+    };
+  },
 
-let id = "main-area";
+  computed: {
+    isBannerAppear() {
+      return (
+        this.isBannerShow &&
+        !this.isTableShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
 
-let tableMahasiswa =
-  '<section id="table-area">' +
-  '<div class="breadcumb">' +
-  "<h1>Data Mahasiswa</h1> " +
-  "</div> " +
-  '<table id="table" class="table-wrap"> ' +
-  '<thead class="table-head"> ' +
-  "<tr> " +
-  "<th>Nama</th> " +
-  "<th>Nim</th> " +
-  "<th>Fakultas</th> " +
-  "<th>Prodi</th> " +
-  "<th>Tahun Masuk</th> " +
-  "<th>Aksi</th> " +
-  "</tr> " +
-  "</thead> " +
-  '<tbody class="table-body"> ' +
-  "<tr> " +
-  "<td>Maulana Sodiqin</td> " +
-  "<td>41037006200028</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>2020</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Fenny Oktaviani</td> " +
-  "<td>41037006200034</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>2020</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Astie Noer Hadiyanti</td> " +
-  "<td>41037006200031</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>2020</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Lutfi Husaeri</td> " +
-  "<td>41037006200006</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>2020</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Kirwan Herdiansyah</td> " +
-  "<td>41037006200007</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>2020</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Desi Suci Nopianti</td> " +
-  "<td>41037006200019</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>2020</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "</tbody> " +
-  "</table> " +
-  "</section>";
+    isTableAppear() {
+      return (
+        this.isTableShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
 
-let tableDosen =
-  '<section id="table-area">' +
-  '<div class="breadcumb">' +
-  "<h1>Data Dosen Pengampu</h1> " +
-  "</div> " +
-  '<table id="table" class="table-wrap"> ' +
-  '<thead class="table-head"> ' +
-  "<tr> " +
-  "<th>Nama</th> " +
-  "<th>Nip</th> " +
-  "<th>Fakultas</th> " +
-  "<th>Jumlah Matkul</th> " +
-  "<th>Aksi</th> " +
-  "</tr> " +
-  "</thead> " +
-  '<tbody class="table-body"> ' +
-  "<tr> " +
-  "<td>Soecipto S.Kom, M.kom</td> " +
-  "<td>1111978273</td> " +
-  "<td>Teknik</td> " +
-  "<td>3</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Isbandi S.Kom, M.Kom</td> " +
-  "<td>3454654634</td> " +
-  "<td>Teknik</td> " +
-  "<td>2</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "</tbody> " +
-  "</table> " +
-  "</section>";
+    isTableMahasiswaAppear() {
+      return (
+        this.isTableShow &&
+        this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        !this.isTableFakultasShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
+    isTableDosenAppear() {
+      return (
+        this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        !this.isTableFakultasShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow &&
+        !this.isBannerShow
+      );
+    },
+    isTableMatakuliahAppear() {
+      return (
+        this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        this.isTableMatakuliahShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
+    isTableFakultasAppear() {
+      return (
+        this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        this.isTableFakultasShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
 
-let tableMatkul =
-  '<section id="table-area">' +
-  '<div class="breadcumb">' +
-  "<h1>Data Mata Kuliah</h1> " +
-  "</div> " +
-  '<table id="table" class="table-wrap"> ' +
-  '<thead class="table-head"> ' +
-  "<tr> " +
-  "<th>Nama</th> " +
-  "<th>Kode</th> " +
-  "<th>Fakultas</th> " +
-  "<th>Prodi</th> " +
-  "<th>Jam</th> " +
-  "<th>SKS</th> " +
-  "<th>Dosen Pengampu</th> " +
-  "<th>Aksi</th> " +
-  "</tr> " +
-  "</thead> " +
-  '<tbody class="table-body"> ' +
-  "<tr> " +
-  "<td>Basis Data</td> " +
-  "<td>TIF866</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>08.00 - 10.00</td> " +
-  "<td>4</td> " +
-  "<td>Soecipto</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "<tr> " +
-  "<td>Pemograman Web</td> " +
-  "<td>TIF6667</td> " +
-  "<td>Teknik</td> " +
-  "<td>Informatilka</td> " +
-  "<td>10.30 - 13.00</td> " +
-  "<td>4</td> " +
-  "<td>Soecipto</td> " +
-  "<td> " +
-  '<button class="table-btn edit">Edit</button> ' +
-  '<button class="table-btn delete">Hapus</button> ' +
-  "</td> " +
-  "</tr> " +
-  "</tbody> " +
-  "</table> " +
-  "</section>";
+    isEditMahasiswaAppear() {
+      return (
+        !this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        !this.isTableFakultasShow &&
+        !this.isBannerShow &&
+        this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
 
-let home =
-  ' <section id="banner-area"> ' +
-  '<div class="banner-wrap">' +
-  '<div class="banner-text">' +
-  '<h1 class="banner-title">' +
-  "Selamat Datang <br />" +
-  "di PSU University" +
-  "</h1>" +
-  "</div>" +
-  '<div class="banner-img">' +
-  '<img src="./assets/img/banner/banner-2.jpg" alt="Banner" />' +
-  "</div>" +
-  "</div>" +
-  " </section>";
+    isEditMatakuliahAppear() {
+      return (
+        !this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        !this.isTableFakultasShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
 
-function getTableMahasiswa() {
-  getId(id, tableMahasiswa);
-}
+    isEditDosenAppear() {
+      return (
+        !this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        !this.isTableFakultasShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        !this.isEditFakultasShow
+      );
+    },
 
-function getTableMatkul() {
-  getId(id, tableMatkul);
-}
+    isEditFakultasAppear() {
+      return (
+        !this.isTableShow &&
+        !this.isTableMahasiswaShow &&
+        !this.isTableDosenShow &&
+        !this.isTableMatakuliahShow &&
+        !this.isTableFakultasShow &&
+        !this.isBannerShow &&
+        !this.isEditMahasiswaShow &&
+        !this.isEditDosenShow &&
+        !this.isEditMatakuliahShow &&
+        this.isEditFakultasShow
+      );
+    },
+  },
 
-function getTableDosen() {
-  getId(id, tableDosen);
-}
+  methods: {
+    getBanner() {
+      this.isBannerShow = true;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isTableShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
 
-function getHome() {
-  getId(id, home);
-}
+    getTableMahasiswa() {
+      this.isTableShow = true;
+      this.isTableMahasiswaShow = true;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
+
+    getTableDosen() {
+      this.isTableShow = true;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = true;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
+
+    getTableMatakuliah() {
+      this.isTableShow = true;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = true;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
+
+    getTableFakultas() {
+      this.isTableShow = true;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = true;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
+
+    getEditFakultas() {
+      this.isTableShow = false;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = true;
+    },
+
+    getEditMahasiswa() {
+      this.isTableShow = false;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = true;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
+
+    getEditDosen() {
+      this.isTableShow = false;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = true;
+      this.isEditMatakuliahShow = false;
+      this.isEditFakultasShow = false;
+    },
+
+    getEditMatakuliah() {
+      this.isTableShow = false;
+      this.isTableMahasiswaShow = false;
+      this.isTableDosenShow = false;
+      this.isTableMatakuliahShow = false;
+      this.isTableFakultasShow = false;
+      this.isBannerShow = false;
+      this.isEditMahasiswaShow = false;
+      this.isEditDosenShow = false;
+      this.isEditMatakuliahShow = true;
+      this.isEditFakultasShow = false;
+    },
+  },
+});
+
+app.mount("#app");
